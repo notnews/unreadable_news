c92acb5c3041e42b6d97ecd67b75aba726b373f9
Lucas Shen <lucas@lucasshen.com>
2022-03-22 20:23:29 +0800

    update figs w/ more cutoffs & data coverage

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,13 @@
 import textstat
 from lexicalrichness import LexicalRichness
+import pandas as pd
+import numpy as np
+import datetime
+import july
+from typing import Optional, List, Union, Tuple
+from matplotlib.pyplot import Axes
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import matplotlib as mpl
 import statsmodels.api as sm
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
@@ -50,9 +58,83 @@ def its(row, base, x):
     else:
         return -1 * abs_change
 
+
+def update_rcparams(
+    fontfamily="monospace",
+    fontsize=12,
+    fontweight='bold',
+    labelsize="medium",
+    titlesize="large",
+    titlepad=10,
+):
+
+    mpl.rcParams.update({
+        'font.size': fontsize,
+        'font.weight': fontweight,
+        'font.family': fontfamily,
+        'axes.labelsize': labelsize,
+        'axes.titlesize': titlesize,
+        'axes.titlepad': titlepad
+        
+    })
+
+
+def coverage_dowmonth(
+    dates: List[Union[str, datetime.date, datetime.datetime]],
+    data: List[float],    
+    figsize: Tuple = (12,5),
+    cmap: Union[str, LinearSegmentedColormap, ListedColormap] = sns.color_palette("mako_r", as_cmap=True),
+    title: Optional[str] = None,
+     **kwargs
+) -> Axes:
     
-def plotline(x, y, title=None, yrange=None, xrange=range(0, 253, 36), xticklabels=None, color=BLUE, figsize=(12*.7, 8*.7), tickersize=13,
-             markersize=80, labsize=15, titlesize=18, bw=0.4, savepath=None):
+    update_rcparams(**kwargs)
+    
+    _, ax = plt.subplots(figsize=figsize)
+    july.heatmap(dates, data, 
+                 title=title, 
+                 cmap=cmap, 
+                 ax=ax)
+
+    return ax
+
+
+def coverage_mthyr(
+    data: List[Union[pd.DataFrame, np.ndarray]],
+    figsize: Tuple = (9,6),
+    figsizescale: float = .95,
+    cmap: Union[str, LinearSegmentedColormap, ListedColormap] = 'mako_r',
+    linewidths: float = .1,
+    ylabel: Optional[str] = None,
+    xlabel: Optional[str] = None,
+    title: Optional[str] = None,
+    xtickangle: Optional[float] = None,
+     **kwargs
+) -> Axes:
+    
+    update_rcparams(**kwargs)
+    
+    figsize = ([x*figsizescale for x in figsize])
+    _, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(data, 
+                linewidths=linewidths, 
+                cmap=cmap,
+                ax=ax)
+    
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.title(title)
+
+    plt.yticks(rotation=0) 
+    if xtickangle:
+      plt.xticks(rotation=xtickangle, ha='right') 
+
+    return ax
+
+      
+def plotline(x, y, title=None, yrange=None, xrange=range(0, 253, 36), 
+    xticklabels=range(1987, 2010, 3), color=BLUE, figsize=(12*.7, 8*.7), tickersize=13,
+    markersize=80, labsize=15, titlesize=18, bw=0.4, savepath=None):
     
     # Get custom lowess
     lowess = sm.nonparametric.lowess(y, x, frac=bw)
@@ -62,7 +144,7 @@ def plotline(x, y, title=None, yrange=None, xrange=range(0, 253, 36), xticklabel
     ynew_line = f(x)
 
     # Init fig
-    fig, ax = plt.subplots(figsize=figsize)
+    _, ax = plt.subplots(figsize=figsize)
 
     # sns.regplot(x=x, y=y, 
     #             color=BLUE, 
@@ -114,7 +196,7 @@ def plot_dual_indices(y1,y2,x, title=None, yrange=None, xrange=None, color1=PURP
                       ylabel='Percentage change from 1987',
                       linewidth=3, tickersize=13, alpha=.6, labsize=15, titlesize=18, savepath=None):
     
-    fig, ax = plt.subplots(figsize=figsize)
+    _, ax = plt.subplots(figsize=figsize)
 
     sns.lineplot(x=x, y=y1, 
                  marker='o',
@@ -159,3 +241,10 @@ def plot_dual_indices(y1,y2,x, title=None, yrange=None, xrange=None, color1=PURP
 
     
     return ax
+
+
+
+def save_mpl_fig(savepath):
+    plt.savefig(f'{savepath}.pdf', dpi=None, bbox_inches='tight', pad_inches=0)
+    plt.savefig(f'{savepath}.png', dpi=120, bbox_inches='tight', pad_inches=0)    
+
